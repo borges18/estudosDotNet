dccd17562410c728f772e65148360dae00fca534
Mateus Borges <mateusbmsilva@gmail.com>
2023-03-20 19:08:17 -0300

    aula 3

---
+++ 1.olaMundo/5.while/while/while/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace While
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            //int contador = 0;
+            //while (contador <= 10)
+            //{
+            //Console.WriteLine($"O contador está em {contador}.");
+            //contador++;
+            //}
+
+            //sintaxe do "for"
+            for (int contador = 0; contador <= 10; contador++)
+            {
+                Console.WriteLine($"O contador está em {contador}.");
+            }
+
+
+
+
+        }
+    }
+}
