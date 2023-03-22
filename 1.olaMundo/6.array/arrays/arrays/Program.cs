8dac96fd99d5b7016ca3d8dd6045eb464b6e675f
Mateus Borges <mateusbmsilva@gmail.com>
2023-03-22 20:07:50 -0300

    aula 4

---
+++ 1.olaMundo/6.array/arrays/arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            var nomes = new List<string> { "Mateus", "Maurício", "Simone" };
+
+            nomes.Add("Lucas");
+
+            nomes.Sort(); //este comando vai classificar em ordem alfabética antes de imprimir na tela.
+            foreach (var nome in nomes)
+            {
+                Console.WriteLine($"Olá {nome}");
+            }
+
+            //a variável "index", é o nome dado para a posição dentro das listas.
+            var index = nomes.IndexOf("kk");
+
+            if (index == -1 )
+            {
+                Console.WriteLine($"Quando o item não for encontrado, IndexOf retornará {index}. ");
+            }
+            else
+            {
+                Console.WriteLine($"O nome {nomes[index]} esta na posição {index}. ");
+            }
+
+        }
+    }
+}
