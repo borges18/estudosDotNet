86fd3595291ca3e15c0fc1ab0dba04a48a529a7f
Mateus Borges <mateusbmsilva@gmail.com>
2023-03-22 21:13:06 -0300

    aula 5

---
+++ 1.olaMundo/7.app1/MeuSuperBanco/MeuSuperBanco/ContaBanco.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuSuperBanco
+{
+    public class ContaBanco
+    {
+        public string Numero { get; }
+        public string Dono { get; set;  }
+        public decimal Saldo 
+        {
+            get
+            {
+                decimal saldo = 0;
+                foreach (var item in todasTransacoes)
+                {
+                    saldo += item.Valor;
+                }
+                return saldo;
+            }
+        }
+
+        public static int numeroConta = 1234567890; 
+
+        private List<Transacao> todasTransacoes = new List<Transacao>();
+
+        public ContaBanco(string nome, decimal valor) 
+        {
+            this.Dono = nome;
+            numeroConta++;
+            this.Numero = numeroConta.ToString();
+
+            Depositar(valor, DateTime.Now, "Saldo inicial: ");
+        }
+
+        public void Depositar(decimal valor, DateTime data, string obs)
+        {
+            if (valor <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(valor), "Não é possível fazer um depósito igual ou menor a R$ 0,00");
+            }
+            Transacao trans = new Transacao(valor, data, obs);
+            todasTransacoes.Add(trans);
+        }
+
+        public void Sacar(decimal valor, DateTime data, string obs)
+        {
+            if (valor <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(valor), "Não é possível fazer um saque igual ou menor a R$ 0,00");
+            }
+
+            if  (Saldo - valor < 0)
+            {
+                throw new InvalidOperationException("Essa operação não é permitida");
+            }
+            Transacao trans = new Transacao(-valor, data, obs);
+            todasTransacoes.Add(trans);
+        }
+
+
+
+
+    }
+
+}
---
+++ 1.olaMundo/7.app1/MeuSuperBanco/MeuSuperBanco/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MeuSuperBanco
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            MeuSuperBanco.ContaBanco conta1 = new ContaBanco("Mateus", 25000);
+            Console.WriteLine($"A conta {conta1.Numero} de {conta1.Dono} foi criada e possui o saldo de R${conta1.Saldo}. ");
+
+            conta1.Sacar(2500, DateTime.Now, "Sacando");
+            Console.WriteLine($"Após a transação o saldo atual é de R${conta1.Saldo}. ");
+
+            conta1.Sacar(30000, DateTime.Now, "Sacando");
+            Console.WriteLine($"Após a transação o saldo atual é de R${conta1.Saldo}. ");
+
+        }
+    }
+}
